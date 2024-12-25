8801f61bae007da0c6c34db80ddb2631fe3c2648
saidiAbir <153833956+saidiAbir@users.noreply.github.com>
2024-12-25 21:59:41 +0100

    first commit

---
+++ src/main/java/com/abir/bijoux/Oauth2GitHubApplication.java
@@ -0,0 +1,13 @@
+package com.abir.bijoux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2GitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2GitHubApplication.class, args);
+	}
+
+}
